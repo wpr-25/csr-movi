9626cfe2e56f87b7dfa5697171553b748c1ca87d
wpr-25 <willy.ok1025@gmail.com>
2023-03-24 15:55:36 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -217,7 +217,7 @@ export default function Home() {
             <div className="max-w-7xl mx-auto text-center text-4xl">
               APRECIATION
               <video width={750} height={200} controls={"autoplay"} className="mx-auto rounded-lg py-6">
-                <source  src="C0047.mp4"/>
+                <source  src="csr-update.mp4"/>
                 <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions"/>
               </video>
             </div>
